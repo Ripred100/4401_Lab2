041fe5f541d284baa30b8dfd777041ed122ee436
asaenger <asaenger@WE-3CP3435-001.eng.uwo.ca>
2022-10-21 09:53:44 -0400

    Created Repo

---
+++ temp.m
@@ -0,0 +1,8 @@
+
+
+%my_bot.plot([ -2*pi/3 5*pi/2 pi/3 pi/5 pi/4 pi/3])
+% 
+% deg = rad2deg([pi, pi,pi,pi,pi,pi,pi])
+% Robot = MSE4401BOT(1234,4321);
+% %Robot.sendPosition(deg)
+% Robot.sendPosition([150,150,150,150,150,150,178])
